4d0011feaae0534c997533e26467d95fd357c443
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2022-04-16 23:41:22 +0200

    RoleController i getAll endpoint.

---
+++ src/main/java/com/pris/cinema/controllers/RoleController.java
@@ -0,0 +1,23 @@
+package com.pris.cinema.controllers;
+
+import com.pris.cinema.entities.User;
+import com.pris.cinema.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/roles")
+public class RoleController {
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @GetMapping("")
+    public ResponseEntity<?> getAll() {
+        return new ResponseEntity<>(roleRepository.findAll(), HttpStatus.OK);
+    }
+}
