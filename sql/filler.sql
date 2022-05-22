4ef6ad95ccb58d651c3995dfe9393ef5ebf5e0ba
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2022-05-22 12:54:11 +0200

    Filler.sql update-ovan.

--- sql/filler.sql
+++ sql/filler.sql
@@ -16,14 +16,14 @@ INSERT INTO `pris_cinema`.`section` (`id`, `section`) VALUES ('2', 'GALLERY_LEFT
 INSERT INTO `pris_cinema`.`section` (`id`, `section`) VALUES ('3', 'GALLERY_RIGHT');
 
 SELECT * FROM movie;
-INSERT INTO `pris_cinema`.`movie` (`id`, `title`, `description`, `image`, `runtime`) VALUES
-('1', 'Predator', 'Dutch and his team are out on a mission to rescue a group of hostages in Central America. There, they discover that they are being targeted by an extraterrestrial warrior.', 'https://upload.wikimedia.org/wikipedia/en/9/95/Predator_Movie.jpg', '107');
-INSERT INTO `pris_cinema`.`movie` (`id`, `title`, `description`, `image`, `runtime`) VALUES
-('2', 'Predator 2', 'Predator 2 is a 1990 American science fiction action film[3] written by brothers Jim and John Thomas, directed by Stephen Hopkins, and starring Danny Glover, Ruben Blades, Gary Busey, María Conchita Alonso, Bill Paxton, and Kevin Peter Hall.', 'https://upload.wikimedia.org/wikipedia/en/b/b5/Predator_two.jpg', '108');
-INSERT INTO `pris_cinema`.`movie` (`id`, `title`, `description`, `image`, `runtime`) VALUES
-('3', 'The Matrix', 'The Matrix is a 1999 science fiction action film[5][6] written and directed by the Wachowskis.', 'https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg', '136');
-INSERT INTO `pris_cinema`.`movie` (`id`, `title`, `description`, `image`, `runtime`) VALUES
-('4', 'Dune', 'In the year 10191, a spice called melange is the most valuable substance known in the universe, and its only source is the desert planet Arrakis. ', 'https://upload.wikimedia.org/wikipedia/en/5/51/Dune_1984_Poster.jpg', '137');
+INSERT INTO `pris_cinema`.`movie` (`id`, `title`, `description`, `image`, `trailer`, `runtime`) VALUES
+('1', 'Predator', 'Dutch and his team are out on a mission to rescue a group of hostages in Central America. There, they discover that they are being targeted by an extraterrestrial warrior.', 'https://upload.wikimedia.org/wikipedia/en/9/95/Predator_Movie.jpg', 'https://www.youtube.com/watch?v=X2hBYGwKh3I', '107');
+INSERT INTO `pris_cinema`.`movie` (`id`, `title`, `description`, `image`, `trailer`, `runtime`) VALUES
+('2', 'Predator 2', 'Predator 2 is a 1990 American science fiction action film[3] written by brothers Jim and John Thomas, directed by Stephen Hopkins, and starring Danny Glover, Ruben Blades, Gary Busey, María Conchita Alonso, Bill Paxton, and Kevin Peter Hall.', 'https://upload.wikimedia.org/wikipedia/en/b/b5/Predator_two.jpg', 'https://www.youtube.com/watch?v=X2hBYGwKh3I', '108');
+INSERT INTO `pris_cinema`.`movie` (`id`, `title`, `description`, `image`, `trailer`, `runtime`) VALUES
+('3', 'The Matrix', 'The Matrix is a 1999 science fiction action film[5][6] written and directed by the Wachowskis.', 'https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg', 'https://www.youtube.com/watch?v=X2hBYGwKh3I', '136');
+INSERT INTO `pris_cinema`.`movie` (`id`, `title`, `description`, `image`, `trailer`, `runtime`) VALUES
+('4', 'Dune', 'In the year 10191, a spice called melange is the most valuable substance known in the universe, and its only source is the desert planet Arrakis. ', 'https://upload.wikimedia.org/wikipedia/en/5/51/Dune_1984_Poster.jpg', 'https://www.youtube.com/watch?v=X2hBYGwKh3I', '137');
 
 SELECT * FROM movie_genre;
 INSERT INTO `pris_cinema`.`movie_genre` (`movie_id`, `genre_id`) VALUES ('1', '1');
